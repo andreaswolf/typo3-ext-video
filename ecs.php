0ac37d6a64d6abf9b9f5601124aa35b070d9291f
Andreas Wolf <dev@a-w.io>
2024-04-22 16:07:15 +0200

    Add easy-coding-standard with configuration

---
+++ ecs.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use PhpCsFixer\Fixer\StringNotation\SingleQuoteFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+
+return ECSConfig::configure()
+    ->withRules([
+        NoUnusedImportsFixer::class,
+        ArraySyntaxFixer::class,
+        SingleQuoteFixer::class,
+    ])
+    ->withPreparedSets(true)
+    ->withPaths([
+        __DIR__ . '/Classes/',
+        __DIR__ . '/Configuration/',
+        __DIR__ . '/Tests/',
+    ])
+    ->withRootFiles()
+    ;
